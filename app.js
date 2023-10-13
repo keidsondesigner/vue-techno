a1522b261dad6690e1711ad1fc69364f5fb31cf9
Keidson Roby <keidsondesigner@gmail.com>
2023-10-13 14:58:27 -0400

    feat: :sparkles: Implementado carrinhoDeCompras e localStorage do Carrinho

--- app.js
+++ app.js
@@ -2,11 +2,23 @@ const vm = new Vue({
   el: "#app",
   data: {
     produtos: [],
-    produto: false
+    produto: false,
+    carrinhoDeCompras: [], 
   },
   filters: {
     formatarMoeda(valor) {
       return valor.toLocaleString("pt-BR", { style: "currency", currency: "BRL"});
+    }
+  },
+  computed: {
+    valorTotalDoCarrinho() {
+      let total = 0;
+      if (this.carrinhoDeCompras.length) {
+        this.carrinhoDeCompras.forEach(item => {
+          total += item.preco;
+        })
+      }
+      return total;
     }
   },
   methods: {
@@ -25,16 +37,48 @@ const vm = new Vue({
         })
     },
     fecharModal(event){
-      console.log("aquiii");
-      console.log(event.target);
-      console.log(event.currentTarget);
-      // if(target === currentTarget) {
-      //   // fechar o modal só quando meu click for na section onde está meu evento;
-      //   this.produto = false;
-      // }
+      // console.log("aquiii");
+      // console.log(event.target);
+      // console.log(event.currentTarget);
+      if(event.target === event.currentTarget) {
+        // fechar o modal só quando meu click for na section onde está meu evento;
+        this.produto = false;
+      }
+    },
+    addItemNoCarrinho() {
+      console.log("carrinho")
+      // Quando adicionar item, deve subitrair do produto.estoque
+      this.produto.estoque--;
+      // Quando adicionar item, deve adicionar no carrinho
+      const { id, nome, preco } = this.produto;
+      console.log(id, nome, preco);
+      this.carrinhoDeCompras.push({id, nome, preco});
+
+    },
+    removerItemDoCarrinho(index) {
+      //Quando Remover item
+      this.carrinhoDeCompras.splice(index, 1);
+    },
+    checarLocalStorage() {
+      if(window.localStorage.carrinho) {
+        // existe "carrinho" no localStorage???
+        this.carrinhoDeCompras = JSON.parse(window.localStorage.carrinho);
+        // transforme "carrinho" que está como "String" para "Array"
+      }
     }
+  },
+  watch: {
+    carrinhoDeCompras() {
+      // Toda vez que houver alteração no carrinhoDeCompras, salve no localStorage;
+      console.log("carrinhoDeCompras teve mudança")
+      // Primeiro vamos transformar nosso Array ou Objeto em uma "String" com JSON.stringfy;
+      // nosso "Key" vai ser "carrinho" & nosso "Value" vai ser "carrinhoDeCompras";
+      window.localStorage.carrinho = JSON.stringify(this.carrinhoDeCompras);
+    }
+
   },
   created() {
     this.getProdutos();
+    this.checarLocalStorage();
   }
 });
